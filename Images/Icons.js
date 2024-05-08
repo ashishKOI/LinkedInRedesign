5d12fd314091cbc59259b3edfb5cb2eefffc2f19
Ashish Giri <ashishgiri@Ashishs-MacBook-Air.local>
2024-05-08 19:49:18 +1000

    add assets

---
+++ Images/Icons.js
@@ -0,0 +1,7 @@
+const linkedin = require("../assets/Linkedin/linkedin.png");
+const gear = require("../assets/Icons/settings.png");
+const people = require("../assets/Icons/people.png");
+const bullet = require("../assets/Icons/bullet.png");
+const message = require("../assets/Icons/message-chat.png");
+
+export default { linkedin, gear, people, bullet, message };
---
+++ Images/Jobs.js
@@ -0,0 +1,7 @@
+const Job1 = require("../assets/Jobs/Job1.png");
+const Job2 = require("../assets/Jobs/Job2.png");
+const Job3 = require("../assets/Jobs/Job3.png");
+const Job4 = require("../assets/Jobs/Job4.png");
+const Job5 = require("../assets/Jobs/Job5.png");
+
+export default { Job1, Job2, Job3, Job4, Job5 };
